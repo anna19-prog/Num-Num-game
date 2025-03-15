e6cbbfb8467f8385b2872b33624c7d7581739136
Viknn8 <viktory.na2610@gmail.com>
2025-03-15 11:42:13 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,77 +1,64 @@
 #include "raylib.h"
 #include <iostream>
-#include <vector>
-#include <stdlib.h> 
+#include <ctime> //используем эту библиотеку так как встроенная функция rand выдает псевдослучайные числа,
+                // которые на самом деле они и те же (мы же устанавливаем нулевое стартовое число и каждай раз получаем разный рандом srand(time(0));)
+const int screenWidth = 2000;
+const int screenHeight = 1500;
+const int playerWidth = 100;
+const int playerHeight = 100;
+const int fallingWidth = 60;
+const int fallingHeight = 60;
 
-using std::vector;
-using std::rand;
-
-#define screenWidth  800
-#define screenHeight  450
-
-struct Ball { // скорее всего надо переделать в класс..
-	Vector2 ballPosition = { 200.0f, 0.0f }; //static_cast<float>(rand()) % (screenWidth - 20)
-	float ballSpeed = 2.0f ;
-	int ballRadius = 10;
-	bool active = 1; // жив или нет
+struct FallingObject {
+    float x;
+	float y;
+    float speed;
+    Texture2D picture;
 };
+FallingObject CreateFallingObject(Texture2D pic) {
+    FallingObject obj;
+    obj.x = rand() % (screenWidth - fallingWidth); // мудрая техника вычисления рандома в опр диапазоне
+    obj.y = 0;
+    obj.speed = rand() % 100 + 200.0f;
+    obj.picture = pic;
+    return obj;
+}
 
+int main() {
+    srand(time(0));
+    InitWindow(screenWidth, screenHeight, "kto kak rabotaet, tot tak i est");
+    SetTargetFPS(60);
+	Texture2D IlyaPicture = LoadTexture("src/pictures/photoIlya.jpg");
+	Texture2D BurgerPicture = LoadTexture("src/pictures/Burger.png");
+    int score = 0;
+    float iluhaX = screenWidth / 2 - playerWidth / 2;
+    const float playerSpeed = 300.0f;
+    FallingObject burger = CreateFallingObject(BurgerPicture); //Тут бургер уже успешно создан
 
-int main(void)
-{
-    SetConfigFlags(FLAG_MSAA_4X_HINT);
-    InitWindow(screenWidth, screenHeight, "meow meow meow");
-
-    //Vector2 ballPosition = { GetScreenWidth()/2.0f, GetScreenHeight() };
-    //Vector2 ballSpeed = { 2.0f, 2.0f };
-    //int ballRadius = 20; // перенесла инициализацию мячиков в структуру
-	vector<Ball> balls; // список мячиков
-	Ball ball1;
-	balls.push_back(ball1);
-
-    bool pause = 0;
-    int framesCounter = 0;
-
-    SetTargetFPS(60);              
-
-    while (!WindowShouldClose())    // Detect window close button or ESC key
-    {
-        if (IsKeyPressed(KEY_SPACE)) pause = !pause;
-
-        if (!pause)
-        {
-            for (int i = 0; i < balls.size(); ++i) {
-				balls[i].ballPosition.y += balls[i].ballSpeed;
-				if (balls[i].ballPosition.y >= (screenHeight - balls[i].ballRadius)) { // если до пола долетел
-					balls[i].ballSpeed = 0;
-					balls[i].active = 0;
-			}
-			}
+    while (!WindowShouldClose()) {
+        float dt = GetFrameTime();  // Время кадра (нужно для плавного движения)
+        if (IsKeyDown(KEY_LEFT) && iluhaX > 0)
+            iluhaX -= playerSpeed * dt;
+        if (IsKeyDown(KEY_RIGHT) && iluhaX < screenWidth - playerWidth)
+            iluhaX += playerSpeed * dt;
+        burger.y += burger.speed * dt;
+        if ((burger.y + fallingHeight >= screenHeight - playerHeight) &&
+            (burger.x + fallingWidth >= iluhaX) &&
+            (burger.x <= iluhaX + playerWidth)) {
+            score++;
+            burger = CreateFallingObject(BurgerPicture);
         }
-        else framesCounter++;
-		if (framesCounter % 60 == 0) {
-			Ball new_ball { {static_cast<float>(rand() % (screenWidth - 10) + 10), 0.0f}, 2.0f, 10, 1};
-			balls.push_back(new_ball);
-		}
 
-        // Draw
-        //-----------------------------------------------------
+        // Если объект улетел вниз — создаём новый
+        if (burger.y > screenHeight) {
+            burger = CreateFallingObject(BurgerPicture);
+        }
         BeginDrawing();
-
-            ClearBackground(RAYWHITE);
-
-			for (int i = 0; i < balls.size(); ++i) {
-				if (balls[i].active) DrawCircleV(balls[i].ballPosition, (float)balls[i].ballRadius, MAROON);
-			}
-
-			// если пауза -- мигающее сообщение
-			DrawText("PRESS SPACE to PAUSE BALL MOVEMENT", 10, screenHeight - 25, 20, LIGHTGRAY); // может пригодиться
-			if (pause && ((framesCounter/30)%2)) DrawText("PAUSED", 350, 200, 30, GRAY);
-
-            DrawFPS(10, 10);
-
+        ClearBackground({255, 182, 193, 255});
+        DrawText(TextFormat("Score: %d", score), 20, 20, 20, DARKGRAY);
+        DrawTexture(IlyaPicture, iluhaX, screenHeight - playerHeight, WHITE);
+        DrawTexture(burger.picture, burger.x, burger.y, WHITE);
         EndDrawing();
     }
-    CloseWindow();        // Close window and OpenGL context
-    return 0;
+    CloseWindow();
 }
