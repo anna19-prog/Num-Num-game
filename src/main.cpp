89e6110a0fc9cae1bba9f6bceae7f3ec9cb5e6d7
anna19-prog <missannastar@yandex.ru>
2025-03-17 10:11:39 +0300

    not much

--- src/main.cpp
+++ src/main.cpp
@@ -2,8 +2,8 @@
 #include <iostream>
 #include <ctime> //используем эту библиотеку так как встроенная функция rand выдает псевдослучайные числа,
                 // которые на самом деле они и те же (мы же устанавливаем нулевое стартовое число и каждай раз получаем разный рандом srand(time(0));)
-const int screenWidth = 2000;
-const int screenHeight = 1500;
+const int screenWidth = 800;
+const int screenHeight = 600;
 const int playerWidth = 100;
 const int playerHeight = 100;
 const int fallingWidth = 60;
@@ -15,6 +15,7 @@ struct FallingObject {
     float speed;
     Texture2D picture;
 };
+
 FallingObject CreateFallingObject(Texture2D pic) {
     FallingObject obj;
     obj.x = rand() % (screenWidth - fallingWidth); // мудрая техника вычисления рандома в опр диапазоне
@@ -28,37 +29,53 @@ int main() {
     srand(time(0));
     InitWindow(screenWidth, screenHeight, "kto kak rabotaet, tot tak i est");
     SetTargetFPS(60);
+
 	Texture2D IlyaPicture = LoadTexture("src/pictures/photoIlya.jpg");
 	Texture2D BurgerPicture = LoadTexture("src/pictures/Burger.png");
+    Texture2D background = LoadTexture("src/pictures/mac.png");
+    //ImageResize(background, screenWidth, screenHeight);
+
     int score = 0;
     float iluhaX = screenWidth / 2 - playerWidth / 2;
     const float playerSpeed = 300.0f;
-    FallingObject burger = CreateFallingObject(BurgerPicture); //Тут бургер уже успешно создан
+
+    std::vector<FallingObject> objects;
+    //FallingObject burger = CreateFallingObject(BurgerPicture); //Тут бургер уже успешно создан
+    objects.push_back(CreateFallingObject(BurgerPicture));
 
     while (!WindowShouldClose()) {
         float dt = GetFrameTime();  // Время кадра (нужно для плавного движения)
         if (IsKeyDown(KEY_LEFT) && iluhaX > 0)
             iluhaX -= playerSpeed * dt;
+
         if (IsKeyDown(KEY_RIGHT) && iluhaX < screenWidth - playerWidth)
             iluhaX += playerSpeed * dt;
-        burger.y += burger.speed * dt;
-        if ((burger.y + fallingHeight >= screenHeight - playerHeight) &&
+
+        for (FallingObject burger: objects) { // обновляем все падающие бургеры
+            burger.y += burger.speed * dt;
+
+            if ((burger.y + fallingHeight >= screenHeight - playerHeight) &&
             (burger.x + fallingWidth >= iluhaX) &&
             (burger.x <= iluhaX + playerWidth)) {
-            score++;
-            burger = CreateFallingObject(BurgerPicture);
+                score++;
+                burger = CreateFallingObject(BurgerPicture);
+            }
+
+            // Если объект улетел вниз — создаём новый
+            if (burger.y > screenHeight) {
+                burger = CreateFallingObject(BurgerPicture);
+            }
         }
 
-        // Если объект улетел вниз — создаём новый
-        if (burger.y > screenHeight) {
-            burger = CreateFallingObject(BurgerPicture);
-        }
+        
         BeginDrawing();
         ClearBackground({255, 182, 193, 255});
+        DrawTexture(background, 0, 0, WHITE);
         DrawText(TextFormat("Score: %d", score), 20, 20, 20, DARKGRAY);
         DrawTexture(IlyaPicture, iluhaX, screenHeight - playerHeight, WHITE);
-        DrawTexture(burger.picture, burger.x, burger.y, WHITE);
+        for (FallingObject burger: objects) DrawTexture(burger.picture, burger.x, burger.y, WHITE);
         EndDrawing();
     }
+
     CloseWindow();
 }
